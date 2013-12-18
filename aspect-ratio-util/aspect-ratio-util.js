be452a5b623c467ee858a550bc2ecb71651896a6
munkychop <munkychop@gmail.com>
2013-12-18 18:47:25 +0000

    Added the initial build of a basic util to detect orientation.
    Hooks are included for running methods when the aspect ratio changes
    between landscape and portrait.

---
+++ aspect-ratio-util/aspect-ratio-util.js
@@ -0,0 +1,154 @@
+function AspectRatioUtil (maxLandscapeMobileWidth, portraitCallback, landscapeCallback, useResizeInsteadOfOrientationchange)
+{
+	// ------------------------------------------------------------
+	// PRIVATE MEMBER VARIABLES
+	// ------------------------------------------------------------
+	var _self = this,
+		_maxMobileWidth = maxLandscapeMobileWidth,
+		_updateEventType = useResizeInsteadOfOrientationchange === true ? "resize" : "orientationchange",
+		_isMobile = false,
+		_isPortrait = false,
+		_isLandscape = false;
+
+	// ------------------------------------------------------------
+	// PUBLIC MEMBER VARIABLES
+	// ------------------------------------------------------------
+	_self.portraitCallback = portraitCallback;
+	_self.landscapeCallback = landscapeCallback;
+
+	// ------------------------------------------------------------
+	// PRIVATE METHODS
+	// ------------------------------------------------------------
+	function checkAspectRatio (event)
+	{
+		// return immediately if callbacks are not set.
+		if (typeof _self.landscapeCallback !== "function" || typeof _self.portraitCallback !== "function") return;
+
+		var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+			height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+
+		if (width > _maxMobileWidth)
+		{
+			_isMobile = false;
+		}
+		else
+		{
+			_isMobile = true;
+		}
+
+		if (width > height)
+		{
+			// only continue if aspect ratio is not already landscape.
+			if (!_isLandscape)
+			{
+				_isPortrait = false;
+				_isLandscape = true;
+
+				// only run callback when on mobile.
+				if (_isMobile) _self.landscapeCallback ();
+			}
+		}
+		else if (height > width)
+		{
+			// only continue if aspect ratio is not already portrait.
+			if (!_isPortrait)
+			{
+				_isLandscape = false;
+				_isPortrait = true;
+
+				// only run callback when on mobile.
+				if (_isMobile) _self.portraitCallback ();
+			}
+		}
+	}
+
+
+	// ------------------------------------------------------------
+	// PUBLIC API
+	// ------------------------------------------------------------
+	_self.checkIfMobile = function ()
+	{
+		return _isMobile;
+	};
+
+	_self.checkIfPortrait = function ()
+	{
+		return _isPortrait;
+	};
+
+	_self.checkIfLandscape = function ()
+	{
+		return _isLandscape;
+	};
+
+	_self.start = function ()
+	{
+		// immediately check aspect ratio in order to set accurate initial values.
+		checkAspectRatio ();
+
+		window.addEventListener (_updateEventType, checkAspectRatio);
+	};
+
+	_self.stop = function ()
+	{
+		window.removeEventListener (_updateEventType, checkAspectRatio);
+	};
+}
+
+window.AspectRatioUtil = AspectRatioUtil;
+
+
+// ------------------------------------------------------------------------------------------- //
+// AddEventListener shim for older browsers from Mozilla:
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.removeEventListener
+// ------------------------------------------------------------------------------------------- //
+if (!Element.prototype.addEventListener) {
+  var oListeners = {};
+  function runListeners(oEvent) {
+    if (!oEvent) { oEvent = window.event; }
+    for (var iLstId = 0, iElId = 0, oEvtListeners = oListeners[oEvent.type]; iElId < oEvtListeners.aEls.length; iElId++) {
+      if (oEvtListeners.aEls[iElId] === this) {
+        for (iLstId; iLstId < oEvtListeners.aEvts[iElId].length; iLstId++) { oEvtListeners.aEvts[iElId][iLstId].call(this, oEvent); }
+        break;
+      }
+    }
+  }
+  Element.prototype.addEventListener = function (sEventType, fListener /*, useCapture (will be ignored!) */) {
+    if (oListeners.hasOwnProperty(sEventType)) {
+      var oEvtListeners = oListeners[sEventType];
+      for (var nElIdx = -1, iElId = 0; iElId < oEvtListeners.aEls.length; iElId++) {
+        if (oEvtListeners.aEls[iElId] === this) { nElIdx = iElId; break; }
+      }
+      if (nElIdx === -1) {
+        oEvtListeners.aEls.push(this);
+        oEvtListeners.aEvts.push([fListener]);
+        this["on" + sEventType] = runListeners;
+      } else {
+        var aElListeners = oEvtListeners.aEvts[nElIdx];
+        if (this["on" + sEventType] !== runListeners) {
+          aElListeners.splice(0);
+          this["on" + sEventType] = runListeners;
+        }
+        for (var iLstId = 0; iLstId < aElListeners.length; iLstId++) {
+          if (aElListeners[iLstId] === fListener) { return; }
+        }     
+        aElListeners.push(fListener);
+      }
+    } else {
+      oListeners[sEventType] = { aEls: [this], aEvts: [ [fListener] ] };
+      this["on" + sEventType] = runListeners;
+    }
+  };
+  Element.prototype.removeEventListener = function (sEventType, fListener /*, useCapture (will be ignored!) */) {
+    if (!oListeners.hasOwnProperty(sEventType)) { return; }
+    var oEvtListeners = oListeners[sEventType];
+    for (var nElIdx = -1, iElId = 0; iElId < oEvtListeners.aEls.length; iElId++) {
+      if (oEvtListeners.aEls[iElId] === this) { nElIdx = iElId; break; }
+    }
+    if (nElIdx === -1) { return; }
+    for (var iLstId = 0, aElListeners = oEvtListeners.aEvts[nElIdx]; iLstId < aElListeners.length; iLstId++) {
+      if (aElListeners[iLstId] === fListener) { aElListeners.splice(iLstId, 1); }
+    }
+  };
+}
