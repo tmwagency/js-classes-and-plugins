db699c72dcef9426d016d7947cfc899f0753baed
munkychop <munkychop@gmail.com>
2013-12-19 17:33:06 +0000

    Converted to a reusable AudioUtil class :)

--- audio-creation/audio.js
+++ audio-creation/audio.js
@@ -1,56 +1,58 @@
-TMW.Audio = {
-	audioSupport : false,
+function AudioUtil ()
+{
+	var _self = this,
+		_audioSupported = document.createElement("audio").canPlayType ? true : false,
+		_testAudio = "undefined",
+		_canPlayMP3 = "undefined",
+		_canPlayOgg = "undefined",
+		_soundPool = {};
 
-	init : function () {
-		if (document.createElement('audio').canPlayType) {
-			//set var to say we support audio
-			TMW.Audio.audioSupport = true;
-			this.loadAudio();
+	if (_audioSupported)
+	{
+		_testAudio = new Audio(); //generic audio object for testing
 
-			$('#audio-play').on('click', function (e) {
-				e.preventDefault();
-				TMW.Audio.playAudio(TMW.Audio.baciSound)
-			});
-		}
-	},
-
-	playAudio : function (sound) {
-
-		sound.play();
-
-	},
-
-	loadAudio : function () {
-
-		//  =============================
-		//  === AUDIO BITS AND PIECES ===
-		//  =============================
-
-		var audio = new Audio(); //generic audio object for testing
-
-		this.canPlayMP3 = !!audio.canPlayType && audio.canPlayType('audio/mp3') !== "";
-		this.canPlayOgg = !!audio.canPlayType && audio.canPlayType('audio/ogg; codecs="vorbis"') !== "";
-
-		//create and assign our audio files
-		TMW.Audio.baciSound = this.createAudio("/audio/baci", false);
-
-	},
-
-	//generic function to create all new audio elements equal, with preload
-	createAudio : function (audioFile, loopSet) {
-		var tempAudio = new Audio(),
-			audioExt;
-
-		if (this.canPlayMP3) {
-			audioExt = '.mp3';
-		} else if (this.canPlayOgg) {
-			audioExt = '.ogg';
-		}
-
-		tempAudio.setAttribute('src', audioFile + audioExt);
-		tempAudio.preload = 'auto';
-		tempAudio.loop = (loopSet === true ? true : false);
-
-		return tempAudio;
+		_canPlayMP3 = !!_testAudio.canPlayType && _testAudio.canPlayType('audio/mp3') !== "";
+		_canPlayOgg = !!_testAudio.canPlayType && _testAudio.canPlayType('audio/ogg; codecs="vorbis"') !== "";
 	}
+
+
+	_self.createSound = function (audioFileName, loop)
+	{
+		// fail silently if audio is not supported.
+		if (!_audioSupported) return;
+
+		// if we have already created this sound then return it from the object pool.
+		if (typeof _soundPool[audioFileName] !== "undefined")
+		{
+			return _soundPool[audioFileName];
+		}
+
+		var audio = new Audio(),
+			fileExtension;
+
+		if (_canPlayMP3)
+		{
+			fileExtension = ".mp3";
+		}
+		else if (_canPlayOgg)
+		{
+			fileExtension = ".ogg";
+		}
+
+		audio.setAttribute("src", audioFile + fileExtension);
+		audio.preload = "auto";
+		audio.loop = loop === true ? true : false;
+
+		_soundPool[audioFileName] = audio;
+
+		return audio;
+	};
+
+	_self.play = function (sound)
+	{
+		// fail silently if audio is not supported.
+		if (!_audioSupported) return;
+
+		sound.play ();
+	};
 }
